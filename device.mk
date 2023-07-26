e6938ec806596cc2c1d3c3a97f59087b3d4cfae8
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-07-26 16:22:17 +0100

    gale: overlay: Enable LTE+ icon
    * Matches the hardcoded default value in stock
    
    Change-Id: Iadb2a78fdb1fad971ee37e434dba616cfab172b9
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- device.mk
+++ device.mk
@@ -326,6 +326,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlayGale \
     SettingsResOverlayGale \
     FrameworksResOverlayGale \
     TetheringResOverlayGale \
