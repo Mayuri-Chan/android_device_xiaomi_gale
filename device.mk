6f52e51f1dd89dd0d2a00103f91a3e93739a8f85
LuK1337 <priv.luk@gmail.com>
2023-12-06 09:54:05 +0100

    gale: Add liboemcrypto.so to vendor linker config
    This lets com.google.android.widevine.nonupdatable.apex access it.
    
    Change-Id: I4e9654fe6317b722f5fbf43b92abb367c18640ce
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- device.mk
+++ device.mk
@@ -131,6 +131,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# Linker config
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    $(LOCAL_PATH)/configs/linker.config.json
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
