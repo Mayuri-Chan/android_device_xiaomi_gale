6a00ae16b05dde2cc7a7471ddd6731967ff2b448
Stim <153687700+StimLuks87@users.noreply.github.com>
2025-06-19 00:50:48 +0500

    gale: Patch mtkfusionrild to load libutils-v32 * Fixes RIL in Android 16
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- extract-files.py
+++ extract-files.py
@@ -54,6 +54,8 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'
     ): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    'vendor/bin/hw/mtkfusionrild': blob_fixup()
+       .add_needed('libutils-v32.so'),
     'vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service': blob_fixup()
         .replace_needed('android.hardware.power-V2-ndk_platform.so', 'android.hardware.power-V2-ndk.so'),
     (
