737caa904e619c8d9bec64723ae7b562bf28e7a9
claxten10 <claxten10@gmail.com>
2025-07-02 06:40:30 +0000

    gale: Include common MediaTek/Xiaomi FCM
    Change-Id: Iae902c69c61ad014de3bb5319a4d164e4006d052
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,10 +179,11 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    vendor/halcyon/config/device_framework_matrix.xml \
-    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
 # Inherit the proprietary files
 include vendor/xiaomi/gale/BoardConfigVendor.mk
