0e0bfb431cd00b80d3413d039fd5dac7cb1a3556
wulan17 <wulan17@komodos.id>
2025-07-13 12:58:20 +0700

    gale: Patch libsink-mtk to load libshim_sink
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- extract-files.py
+++ extract-files.py
@@ -41,7 +41,8 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService.patch', '-r'),
     'system_ext/lib64/libsink-mtk.so': blob_fixup()
-        .add_needed('libaudioclient_shim.so'),
+        .add_needed('libaudioclient_shim.so')
+        .add_needed('libshim_shink.so'),
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
