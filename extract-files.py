d60f2bf9ef8c5e11521d22cd68e1776cbcb57f27
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-04 02:51:56 +0200

    gale: Make libformatter MTK specific
    * Avoid collisions with Car libraries.
    
    Change-Id: I602b1d65fdf672244a91a3f7c4a71470beae7ea4

--- extract-files.py
+++ extract-files.py
@@ -102,6 +102,11 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so',
     ): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+     (
+        'vendor/lib64/libcodec2_mtk_venc.so',
+        'vendor/lib64/libcodec2_mtk_vdec.so',
+     ): blob_fixup()
+        .replace_needed('libformatter.so', 'libformatter_mtk.so')
 }  # fmt: skip
 
 module = ExtractUtilsModule(
